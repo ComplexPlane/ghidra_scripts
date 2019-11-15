9de79edf76cf80b812a08f0f1e5a7f25725a8b76
ComplexPlane <complexplanegames@gmail.com>
2019-11-15 12:30:40 -0500

    Factor out project dir

--- export.sh
+++ export.sh
@@ -2,5 +2,6 @@
 
 analyze_headless_path=~/build/ghidra_9.1-BETA_DEV_20190923/ghidra_9.1-BETA_DEV/support/analyzeHeadless
 project_path=~/doc/projects/romhack/ghidra-smb2
+folder_path=smb2/gc-sdk
 
-"$analyze_headless_path" "$project_path" smb2/gc-sdk -process '*.o' -recursive -postScript ExportXmlAndBinary.java -max-cpu "$(nproc)"
+"$analyze_headless_path" "$project_path" "$folder_path" -process '*.o' -recursive -postScript ExportXmlAndBinary.java -max-cpu "$(nproc)"
