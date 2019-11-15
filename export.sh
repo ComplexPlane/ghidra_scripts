26185099d262ccbb4c62a5a5e5af2ca7057a4fec
ComplexPlane <complexplanegames@gmail.com>
2019-11-14 21:16:24 -0500

    Factor out some vars in export.sh

--- export.sh
+++ export.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-~/build/ghidra_9.1-BETA_DEV_20190923/ghidra_9.1-BETA_DEV/support/analyzeHeadless ./ghidra-smb2 smb2 -process '*.o' -recursive -postScript ExportXmlAndBinary.java
+analyze_headless_path=~/build/ghidra_9.1-BETA_DEV_20190923/ghidra_9.1-BETA_DEV/support/analyzeHeadless
+project_path=~/doc/projects/romhack/ghidra-smb2
+
+"$analyze_headless_path" "$project_path" smb2/gc-sdk -process '*.o' -recursive -postScript ExportXmlAndBinary.java -max-cpu "$(nproc)"
